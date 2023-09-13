6a4951a16cc37d320d35bf7c7e253530235c4657
irchamali <id.irchamali@gmail.com>
2023-09-13 22:29:06 +0700

    Move post_pagination and update pager config to vendor

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,8 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'post_pagination' => 'App\Views\Pagers\post_pagination'
+        'post_pagination' => 'CodeIgniter\Pager\Views\post_pagination'
+        // 'post_pagination' => 'App\Views\Pagers\post_pagination'
     ];
 
     /**
